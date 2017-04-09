13fb7e590a1161558c189850e2ba395d5da473ea
DevArtemMrsh <mirosh1308@gmail.com>
2017-04-09 22:57:57 +0300

    travellp

--- js/mail.php
+++ js/mail.php
@@ -1,8 +1,13 @@
 <?
-if (isset ($_POST['message'])) {
-  mail ("mirosh1308@gmail.com",
-        "заполнена контактная форма с ".$_SERVER['HTTP_REFERER'],
-        "Имя: ".$_POST['name']."\nEmail: ".$_POST['email']."\nТема: ".$_POST['subject']."\nСообщение: ".$_POST['message']);
-  echo ('<p style="color: green">Ваше сообщение получено, спасибо!</p>');
+if (array_key_exists('message', $_POST)) {
+   $to = 'mirosh1308@gmail.com';
+   $subject = 'Заполнена контактная форма с '.$_SERVER['HTTP_REFERER'];
+   $subject = "=?utf-8?b?". base64_encode($subject) ."?=";
+   $message = "Имя: ".$_POST['nameF']."\nEmail: ".$_POST['email']."\nIP: ".$_SERVER['REMOTE_ADDR']."\nТема: ".$_POST['subject']."\nСообщение: ".$_POST['message'];
+   $headers = 'Content-type: text/plain; charset="utf-8"';
+   $headers .= "MIME-Version: 1.0\r\n";
+   $headers .= "Date: ". date('D, d M Y h:i:s O') ."\r\n";
+   mail($to, $subject, $message, $headers);
+   echo $_POST['nameF'];
 }
 ?>
--- js/script.js
+++ js/script.js
@@ -6,22 +6,124 @@ window.onload = function () {
       menuList.classList.toggle("show_navigation")
    }
    menuBtn.addEventListener("click", showNav)
+
+   // SCROLL-TO-TOP BUTTON SHOWING
+   var topBtn = document.querySelector('.to-top-btn')
+   window.addEventListener("scroll", btnShowing);
+
+   function btnShowing() {
+       var scrollToTop = window.pageYOffset;
+       if (scrollToTop >= 100) {
+           topBtn.classList.add("show_topBtn");
+       } else if (scrollToTop <= 100) {
+           topBtn.classList.remove("show_topBtn")
+       }
+   }
+
+   // ADDING ACTIVE CLASS
+   elems = document.getElementsByClassName("menu_link");
+
+   makeActive = function () {
+       for (var i = 0; i < elems.length; i++) {
+          elems[i].classList.remove('active');
+          this.classList.add('active');
+       }
+   };
+
+   for (var i = 0; i < elems.length; i++)
+       elems[i].addEventListener('mousedown', makeActive);
 }
 
-document.getElementById('feedback-form').addEventListener('submit', function(evt){
+// AJAX Request
+document.getElementById('contacts__form').addEventListener('submit', function(evt){
   var http = new XMLHttpRequest(), f = this;
   evt.preventDefault();
-  http.open("POST", "contacts.php", true);
+  http.open("POST", "mail.php", true);
   http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  http.send("nameFF=" + f.nameFF.value + "&contactFF=" + f.contactFF.value + "&messageFF=" + f.messageFF.value);
+  http.send("name=" + f.nameF.value + "&email=" + f.email.value +"&subject=" + f.subject.value + "&message=" + f.message.value);
   http.onreadystatechange = function() {
     if (http.readyState == 4 && http.status == 200) {
-      alert(http.responseText + ', Ваше сообщение получено.\nНаши специалисты ответят Вам в течении 2-х дней.\nБлагодарим за интерес к нашей фирме!');
-      f.messageFF.removeAttribute('value'); // очистить поле сообщения (две строки)
-      f.messageFF.value='';
+      alert(http.responseText + 'Ваше сообщение получено!');
+      f.message.removeAttribute('value');
+      f.message.value='';
+      f.nameF.removeAttribute('value');
+      f.nameF.value='';
+      f.email.removeAttribute('value');
+      f.email.value='';
+      f.subject.removeAttribute('value');
+      f.subject.value='';
     }
   }
   http.onerror = function() {
     alert('Извините, данные не были переданы');
   }
 }, false);
+
+// SMOOTH SCROLLING INITIALIZE
+function currentYPosition() {
+    // Firefox, Chrome, Opera, Safari
+    if (self.pageYOffset) return self.pageYOffset;
+    // Internet Explorer 6 - standards mode
+    if (document.documentElement && document.documentElement.scrollTop)
+        return document.documentElement.scrollTop;
+    // Internet Explorer 6, 7 and 8
+    if (document.body.scrollTop) return document.body.scrollTop;
+    return 0;
+}
+
+function elmYPosition(eID) {
+    var elm = document.getElementById(eID);
+    var y = elm.offsetTop;
+    var node = elm;
+    while (node.offsetParent && node.offsetParent != document.body) {
+        node = node.offsetParent;
+        y += node.offsetTop;
+    }
+    return y;
+}
+
+
+function smoothScroll(eID) {
+    var startY = currentYPosition();
+    var stopY = elmYPosition(eID);
+    var distance = stopY > startY ? stopY - startY : startY - stopY;
+    if (distance < 100) {
+        scrollTo(0, stopY);
+        return;
+    }
+    var speed = Math.round(distance / 100);
+    if (speed >= 20) speed = 20;
+    var step = Math.round(distance / 40);
+    var leapY = stopY > startY ? startY + step : startY - step;
+    var timer = 0;
+    if (stopY > startY) {
+        for (var i = startY; i < stopY; i += step) {
+            setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
+            leapY += step;
+            if (leapY > stopY) leapY = stopY;
+            timer++;
+        }
+        return;
+    }
+    for (var i = startY; i > stopY; i -= step) {
+        setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
+        leapY -= step;
+        if (leapY < stopY) leapY = stopY;
+        timer++;
+    }
+}
+var elems, makeActive;
+
+
+
+elems = document.querySelectorAll(".menu_link");
+
+makeActive = function () {
+    for (var i = 0; i < elems.length; i++)
+        elems[i].classList.remove('active');
+
+    this.classList.add('active');
+};
+
+for (var i = 0; i < elems.length; i++)
+    elems[i].addEventListener('mousedown', makeActive);
